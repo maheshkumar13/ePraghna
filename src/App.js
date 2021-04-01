a1bd998cc20402c39f4d8f52d4815c5ec350ee16
neeharp <neehar.p@varsitymgmt.com>
2021-04-01 11:27:26 +0530

    Issue Title Fixes #2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <h>Neehar</h> */}
+        {/* <h>Neehar2</h> */}
       </header>
     </div>
   );
